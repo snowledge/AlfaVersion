48f295247f091df54a0badbccb7f02b8bae8361d
snowledge <snowledge@users.noreply.github.com>
2019-05-18 14:44:42 +0300

    Random generation
    I created a random generation of the world(basic location), but I don't know if it's the best solution...

---
+++ Alfa/Assets/CreateWorld.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int x = 0; x < 10; x++)
+        {
+            for (int z = 0; z < 10; z++)
+            {
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.position = new Vector3(x, 0, z);
+            }
+        }
+
+    }
+}
